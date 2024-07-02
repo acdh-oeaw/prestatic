68492274ba669a22f18c96da087d5d7898a23099
Stefan Probst <stefanprobst@posteo.de>
2024-07-02 21:41:16 +0200

    fix: sanitize identifers starting with digit

---
+++ .changeset/thirty-beds-know.md
@@ -0,0 +1,5 @@
+---
+"@acdh-oeaw/prestatic": patch
+---
+
+sanitize identifers starting with digit
--- src/sanitize.ts
+++ src/sanitize.ts
@@ -1,5 +1,6 @@
-const regex = /[^\w$]/g;
+const characters = /[^\w$]/g;
+const first = /^([^a-z])/;
 
 export function sanitize(value: string): string {
-	return value.replace(regex, "_");
+	return value.replace(characters, "_").replace(first, "_$1");
 }

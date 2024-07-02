7a99410b7b06ca1ab7730c7f0e093712c6565e52
Stefan Probst <stefanprobst@posteo.de>
2024-07-02 13:20:51 +0200

    test: use cached processors in tests

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -1,6 +1,6 @@
 import { join } from "node:path";
 
-import { compile as _compile } from "@mdx-js/mdx";
+import { createFormatAwareProcessors } from "@mdx-js/mdx/internal-create-format-aware-processors";
 import withGfm from "remark-gfm";
 import { test } from "uvu";
 import * as assert from "uvu/assert";
@@ -13,9 +13,11 @@ const cwd = join(import.meta.dirname, "fixtures");
 const outputFolderPath = join(import.meta.dirname, ".content");
 
 function compile(file: VFile): Promise<VFile> {
-	return _compile(file, {
+	const processor = createFormatAwareProcessors({
 		remarkPlugins: [withGfm],
 	});
+
+	return processor.process(file);
 }
 
 test("should run without throwing errors", () => {
